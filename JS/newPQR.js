795ee7396d812714ed19853bc3392e39bc425c41
sarquiat <guevaracubillos@gmail.com>
2020-09-15 23:12:55 -0500

    Added JS for creating PQR

---
+++ JS/newPQR.js
@@ -0,0 +1,37 @@
+async function regPQR() {
+    let subject = document.getElementById("asunto").value;
+    let request_type = document.getElementById("tSolicitud").value;
+    alert(request_type);
+    let description = document.getElementById("cuerpo").value;
+
+  if (subject == "" || subject == null || description == "" || description == null) {
+    alert("Todos los espacios deben llenarse");
+    return false;
+  }else{
+    var request = postPQR(subject, request_type, description);
+  }  
+}
+
+async function postPQR(s, r, d){
+
+    let response = await fetch('https://pqr-api-rails.herokuapp.com/requests/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify({
+                user_id: 8,
+                request_type: r,
+                request_state: 'in_progress',
+                description: d,
+                subject: s
+            })
+    });
+
+    if (response.ok) {
+      var pjson = await response.json();    
+      alert("Peticion creada con exito");  
+    } else {
+      alert("HTTP-Error: " + response.status);
+    }
+}
